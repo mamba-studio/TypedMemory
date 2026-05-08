ec16b5e60ef5ffb014ce97de2f23eca20bc52c8f
Joe Mwangi <joemwangimburu@gmail.com>
2026-05-09 02:02:23 +0300

    Update

--- src/test/java/TestMem.java
+++ src/test/java/TestMem.java
@@ -5,6 +5,7 @@
 
 
 import com.mamba.typedmemory.api.Mem;
+import com.mamba.typedmemory.api.MemLayout;
 import com.mamba.typedmemory.api.size;
 import java.lang.foreign.Arena;
 import java.util.Arrays;
@@ -13,18 +14,19 @@ import java.util.Arrays;
  *
  * @author joemw
  */
-public class TestMem {
-    
+public class TestMem {    
     record Pixel(int x, @size(2)double[] y){}
     
     void main(){
         try (var arena = Arena.ofConfined()) {
-            var colors = Mem.of(Pixel.class, arena, 10);
+            var colors = Mem.of(Pixel.class, arena, 1000);
             var pixel = new Pixel(3, new double[]{3, 23});
             colors.set(5, pixel);
             
-            if(colors.get(11) instanceof Pixel(int x, double[] y))
+            if(colors.get(5) instanceof Pixel(int x, double[] y))
                 IO.println(x+ " " +Arrays.toString(y));
+            
+            IO.println(MemLayout.memorySummary(colors));
         }
     }
 }
